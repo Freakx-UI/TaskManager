c477e0fd45cd66e2c3270c15b852a770c46e5228
Freak-JStoychev <gstoychev20@gmail.com>
2024-09-28 00:31:57 +0300

    Add edit button and function to not move from done stage

---
+++ dragAndDrop.js
@@ -0,0 +1,60 @@
+ const DragAndDrop = {
+     applyDragAndDrop() {
+         console.log('this is drag and drop');
+         const taskContainers = document.querySelectorAll('.task-container');
+         const stageContainer = document.querySelectorAll('.stage-containers');
+
+         taskContainers.forEach(task => {
+                 task.addEventListener('dragstart', DragAndDrop.handleDragStart);
+
+             }),
+
+
+             stageContainer.forEach(task => {
+                 task.addEventListener('dragover', DragAndDrop.handleDragOver);
+                 task.addEventListener('drop', DragAndDrop.handleDrop);
+
+             })
+     },
+
+     handleDragStart(event) {
+         event.dataTransfer.setData('text/plain', event.target.dataset.taskId);
+         document.querySelectorAll('.stage-containers').forEach(el => {
+             el.classList.add('drag-border-show');
+         })
+     },
+
+     handleDragOver(event) {
+         event.preventDefault();
+     },
+
+     handleDrop(event) {
+         event.preventDefault();
+         const taskId = event.dataTransfer.getData('text/plain');
+         const task = TaskList.taskItem.find(task => task.id == taskId);
+         const taskClass = event.target.classList;
+         document.querySelectorAll('.stage-containers').forEach(el => {
+             el.classList.remove('drag-border-show');
+         })
+         if (task) {
+             if (task.stage === 'done') {
+                 alert('You cant move task from this stage');
+                 return;
+             }
+         }
+
+         if (task) {
+             if (taskClass.contains('to-do-container')) {
+                 task.stage = 'todo';
+             } else if (taskClass.contains('in-progress-container')) {
+                 task.stage = 'inprogress';
+             } else if (taskClass.contains('on-hold-container')) {
+                 task.stage = 'onhold';
+             } else if (taskClass.contains('done-container')) {
+                 task.stage = 'done';
+             }
+
+             renderHTMl();
+         }
+     }
+ }
--- renderPage.js
+++ renderPage.js
@@ -14,12 +14,16 @@ function renderHTMl() {
             buttonsHtml = `
                     <button class="move-forward task-buttons" data-task-id="${task.id}">
                         <i class="fa-solid fa-forward"></i></button>
+                    <button class="edit task-buttons" data-task-id="${task.id}">
+                        <i class="fa-solid fa-pen-to-square"></i></button>
                     <button class="delete-task-button task-buttons" data-task-id="${task.id}">
                         <i class="fa-solid fa-trash"></i></button>`
         } else if (task.stage === 'inprogress') {
             buttonsHtml = `
                     <button class="move-pause task-buttons" data-task-id="${task.id}">
                         <i class="fa-solid fa-pause"></i></button>
+                    <button class="edit task-buttons" data-task-id="${task.id}">
+                        <i class="fa-solid fa-pen-to-square"></i></button>
                     <button class="move-forward task-buttons" data-task-id="${task.id}">
                         <i class="fa-solid fa-check"></i></button>
                     <button class="delete-task-button task-buttons" data-task-id="${task.id}">
@@ -29,6 +33,8 @@ function renderHTMl() {
             buttonsHtml = `
                     <button class="move-backward task-buttons" data-task-id="${task.id}">
                         <i class="fa-solid fa-backward"></i></button>
+                    <button class="edit task-buttons" data-task-id="${task.id}">
+                        <i class="fa-solid fa-pen-to-square"></i></button>
                     <button class="delete-task-button task-buttons" data-task-id="${task.id}">
                         <i class="fa-solid fa-trash"></i></button>
                 `
@@ -67,61 +73,9 @@ function renderHTMl() {
     document.querySelector('.on-hold-container').innerHTML = onholdHTMl;
     document.querySelector('.done-container').innerHTML = doneHTML;
 
-    applyDragAndDrop();
+    DragAndDrop.applyDragAndDrop();
     TaskList.deleteTask();
     TaskList.forwardTask();
     TaskList.moveToOnhold();
     TaskList.backwardTask();
-}
-
-function applyDragAndDrop() {
-    console.log('this is drag and drop');
-    const taskContainers = document.querySelectorAll('.task-container');
-    const stageContainer = document.querySelectorAll('.stage-containers');
-
-    taskContainers.forEach(task => {
-        task.addEventListener('dragstart', handleDragStart);
-
-    });
-
-    stageContainer.forEach(task => {
-        task.addEventListener('dragover', handleDragOver);
-        task.addEventListener('drop', handleDrop);
-
-    });
-}
-
-function handleDragStart(event) {
-    event.dataTransfer.setData('text/plain', event.target.dataset.taskId);
-    document.querySelectorAll('.stage-containers').forEach(el => {
-        el.classList.add('drag-border-show');
-    });
-}
-
-function handleDragOver(event) {
-    event.preventDefault();
-}
-
-function handleDrop(event) {
-    event.preventDefault();
-    const taskId = event.dataTransfer.getData('text/plain');
-    const task = TaskList.taskItem.find(task => task.id == taskId);
-    const taskClass = event.target.classList;
-    document.querySelectorAll('.stage-containers').forEach(el => {
-        el.classList.remove('drag-border-show');
-    });
-
-    if (task) {
-        if (taskClass.contains('to-do-container')) {
-            task.stage = 'todo';
-        } else if (taskClass.contains('in-progress-container')) {
-            task.stage = 'inprogress';
-        } else if (taskClass.contains('on-hold-container')) {
-            task.stage = 'onhold';
-        } else if (taskClass.contains('done-container')) {
-            task.stage = 'done';
-        }
-
-        renderHTMl();
-    }
 }
